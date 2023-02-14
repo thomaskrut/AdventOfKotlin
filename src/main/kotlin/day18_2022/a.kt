f3d24782b953b1abf761a45d03b6df612bd3bf54
thomaskrut <thomas.krut@yh.nackademin.se>
2023-02-14 12:25:27 +0100

    part 1 solved

--- src/main/kotlin/day18_2022/a.kt
+++ src/main/kotlin/day18_2022/a.kt
@@ -1,25 +1,72 @@
 import java.io.File
 
-class Droplet(val x: Int, val y: Int, val z: Int) {
+data class Droplet(val x: Int, val y: Int, val z: Int) {
 
 
-    override fun toString(): String {
-        return "$x, $y, $z"
+    fun checkForAir(step: Int, dirx : Int = 0, diry: Int = 0, dirz: Int = 0, list: List<Droplet>): Int {
+
+        if (step == 50) return step
+
+        val point = Droplet(this.x + dirx, this.y + diry, this.z + dirz)
+        if (list.contains(point)) return step
+
+        return checkForAir(step + 1, dirx, diry, dirz, list)
+
     }
+
+    fun isAdjacent(input: Droplet, list: List<Droplet>): Boolean {
+
+        if ((input.x == this.x) && (input.y == this.y)) {
+            if ((input.z == this.z + 1) || (input.z == this.z - 1)) {
+
+                if (checkForAir(1, dirz = +1, list = list) == 50 && (checkForAir(1, dirz = -1, list = list) == 50)) return true
+            }
+        }
+
+        if ((input.x == this.x) && (input.z == this.z)) {
+            if ((input.y == this.y + 1) || (input.y == this.y - 1)) {
+                if (checkForAir(1, diry = +1, list = list) == 50 && (checkForAir(1, diry = -1, list = list) == 50)) return true
+            }
+        }
+
+        if ((input.z == this.z) && (input.y == this.y)) {
+            if ((input.x == this.x + 1) || (input.x == this.x - 1)) {
+                if (checkForAir(1, dirx = +1, list = list) == 50 && (checkForAir(1, dirx = -1, list = list) == 50)) return true
+            }
+        }
+
+        return false
+
+    }
+
 }
+
+fun counterNumberOfExposedSides(droplet: Droplet, list: List<Droplet>): Int {
+
+    var result = 0
+
+    list.forEach {
+        if (droplet.isAdjacent(it, list)) result++
+    }
+
+    return 6 - result
+
+}
+
+
 
 fun main() {
 
-    val stringList = File("src/main/kotlin/day18_2022", "inputtest.txt").readLines()
+    val stringList = File("src/main/kotlin/day18_2022", "input.txt").readLines()
 
     val droplets = stringList.map { s ->
-        s.split(",").map {
-          s -> s.toInt()
+        val (x, y, z) = s.split(",").map { it.toInt() }
+        Droplet(x, y, z)
+    }
 
-        }.toList()
+    val totalNumberOfExposedSides = droplets.sumOf { d -> counterNumberOfExposedSides(d, droplets) }
+
+    println(totalNumberOfExposedSides)
 
 
-    }.toList()
-
-    println(droplets)
 }
---
+++ src/test/kotlin/day18_2022/AKtTest.kt
@@ -0,0 +1,32 @@
+package day18_2022
+
+import Droplet
+import counterNumberOfExposedSides
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class AKtTest {
+
+    @Test
+    fun testCountNumberOfExposedSides() {
+
+        val stringList = File("src/main/kotlin/day18_2022", "inputtest.txt").readLines()
+
+        val droplets = stringList.map { s ->
+            val (x, y, z) = s.split(",").map { it.toInt() }
+            Droplet(x, y, z)
+        }
+
+
+        assertTrue(Droplet(1,1,1).isAdjacent(Droplet(1,1,0)))
+
+
+        val j = droplets.sumOf { d -> counterNumberOfExposedSides(d, droplets)}
+
+        assertEquals(j, 64)
+
+    }
+
+
+}
