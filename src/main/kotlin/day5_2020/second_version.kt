284cab2cee428378093df2c669400e8476f05e1b
thomaskrut <thomas.krut@yh.nackademin.se>
2023-02-20 20:55:25 +0100

    comments

---
+++ src/main/kotlin/day5_2020/second_version.kt
@@ -0,0 +1,59 @@
+/************************* --- Day 5: Binary Boarding --- **************************
+
+https://adventofcode.com/2020/day/5
+
+Baserat på en sträng bokstäver (front, back, left, right) ska man sortera
+ut rätt platsnummer på ett flygplan. Först delas raderna in i främre eller
+bakre halvan av flygplanet, sedan den halvan i främre eller bakre raden,
+osv tills bara en rad återstår. Sedan gör man likadant med "kolumnera" och
+hittar rätt platsnummer.
+
+För del 1 ska man sortera ut alla platsnummer på
+alla boardingpass och hitta det högsta ID-numret.
+
+För del 2 ska man hitta sitt egen nummer (man har tappat sitt eget boardingpass)
+genom att kolla vilket som inte finns med i den långa listan med alla nummer.
+
+ ************************************************************************************/
+
+package day5_2020
+
+import java.io.File
+
+fun getIdByBinary(s: String): Int {
+
+    return s.map {
+        when(it) {
+            'F','L' -> 0
+            else -> 1
+        }
+    }.joinToString("").toInt(2)
+
+}
+
+
+fun main() {
+
+    val stringList = File("src/main/kotlin/day5_2020", "input.txt").readLines()
+
+    // Lösning del 1: Inspirerad av Todd Ginsbergs variant, där han konverterar bokstäverna till 1:or och 0:or
+    // och sedan omvandlar det resulterande binära talet till decimalform. Upptäckten att bokstäverna i pusslet gick
+    // att direktöversätta till binär talbas är naturligtvis hans upptäckt, men gav mig möjlighet att testa metoderna
+    // joinToString och toInt(talbas) i min egen getId-metod som implementeras ovan. Lösningen eliminerar helt behovet
+    // av mina två hjälpmetoder getRow() och getColumn()
+    // https://todd.ginsberg.com/post/advent-of-code/2020/day5/
+
+    println(stringList.map {
+        getIdByBinary(it)
+    }.max())
+
+    // Lösning del 2 - min egen lösning hade inget sätt att skriva ut det värde som saknades i listan,
+    // utan skrev istället ut de angränsande värdena. Jag hittade metoden first() i Todd Ginsbergs lösning, som precis
+    // som filter tar ett predicate men returnerar den första instans då villkoret uppfylls. Jag använder mitt tidigare predicate
+    // igen, och tar resultatet och adderar 1, så får jag rätt svar.
+    // https://todd.ginsberg.com/post/advent-of-code/2020/day5/
+
+    println(stringList.map { getIdByBinary(it) }.sorted().windowed(2, 1).first { l -> (l[1] == (l[0] + 2)) }.min() + 1)
+
+
+}
---
+++ src/main/kotlin/day9_2020/my_version.kt
@@ -0,0 +1,70 @@
+/******************************* --- Day 9: Encoding Error --- *******************************
+ *
+ * Här ska man hitta det första talet i en sekvens som inte är summan av två av talen i de
+ * senaste 25 talen av sekvensen (senaste 5 för testinputen). De två talen måste vara olika.
+ *
+ * För del 1 räcker det att hitta första talet.
+ *
+ * För del 2 ska man hitta en sammanhängande sekvens av tal som tillsammans utgör summan
+ * av talet som hittades i del 1, och sedan lägga ihop det lägsta och högsta talet i denna
+ * sekvens.
+ *
+ *********************************************************************************************/
+
+package day9_2020
+
+import java.io.File
+
+fun isValid(input: Long, list: List<Long>): Boolean {
+
+    return list.any { x ->
+        (list.filter { y -> x + y == input && x != y }).any()
+    }
+
+}
+
+fun findRange(sumToFind: Long, list: List<Long>): List<Long> {
+
+    list.forEachIndexed { index, _ ->
+
+        for (i in index..list.size) {
+
+            if (list.subList(index, i).sum() == sumToFind) return list.subList(index, i)
+
+        }
+
+    }
+    return listOf(-1)
+}
+
+
+fun main() {
+    val stringList = File("src/main/kotlin/day9_2020", "input.txt").readLines()
+
+    // Lösning del 1:
+    // Varje tal görs först om till long. Sedan plockar funktionen windowed ut 26 element i taget, och vart
+    // och ett av dessa skickas sedan tillsammans med delmänged av listan till min funktion isValid()
+    // som kollar av det mot resten av listan och ger true om summan av dem är lika med sista talet i listan.
+    // Om det är false har vi hittat rätt svar.
+
+    var result = 0L
+
+    stringList.map { it.toLong() }.windowed(26, 1).forEach {
+        if (!isValid(it.takeLast(1)[0], it.dropLast(1))) {
+            result = it.takeLast(1)[0]
+        }
+    }
+
+    println(result)
+
+    // Lösning del 2:
+    // Del två löses med metoden findRange(), som tar den summa som eftersöks samt en lista med Longs att kolla igenom.
+    // Det blev en for-loop som del av lambdan, vilket kanske inte är det elegantaste sättet, där listan gås igenom
+    // en gång utifrån varje index i listan. Om ett range hittas vars summa motsvarar talet som eftersöks returneras detta
+    // som en lista, som sparas i variabeln nedan.
+
+    val range = findRange(result, stringList.map { it.toLong() })
+
+    println(range.max() + range.min())
+
+}
